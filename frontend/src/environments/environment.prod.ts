458254907a30fcbaa66ccf6547c51edcf9d4ee50
Lászlo Caputo <programadorcaputo@gmail.com>
2022-03-01 10:48:20 -0500

    update ip on front environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://facturacion.og23.co:8000/api/v1'
+  API_URL: 'http://137.184.192.104:8000/api/v1'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'https://facturacion.og23.co:8000/api/v1',
+  API_URL: 'http://137.184.192.104:8000/api/v1',
 };
 
 /*
