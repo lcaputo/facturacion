66afd826a3866f06fbfa1db2790a95a9cfc7f5f5
Lászlo Caputo <programadorcaputo@gmail.com>
2022-03-01 14:50:14 -0500

    Update run.sh

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash/
-pm2 start "ng serve --host 0.0.0.0 --port 80 --disable-host-check" --name 'og23-app'
+pm2 start "ng serve --host 0.0.0.0 --port 80 --disable-host-check --live-reload false" --name 'og23-app'
