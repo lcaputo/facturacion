6f304f3a7cd2e1a6b26f5369f9d24751e797b091
Lászlo Caputo <programadorcaputo@gmail.com>
2022-03-01 15:45:28 -0500

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-python manage.py migrate --no-input
+python3 manage.py migrate --no-input
 
-python manage.py collectstatic --no-input
+python3 manage.py collectstatic --no-input
 
-python manage.py makemessages -l es
+python3 manage.py makemessages -l es
 
-python manage.py compilemessages
+python3 manage.py compilemessages
 
 gunicorn config.wsgi --bind 0.0.0.0:8000
