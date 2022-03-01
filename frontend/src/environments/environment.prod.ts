b79290a5a7c996d233fa329b56eb0b5398a089c6
Lászlo Caputo <programadorcaputo@gmail.com>
2022-03-01 10:02:16 -0500

    update api url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:8000/api/v1'
+  API_URL: 'https://facturacion.og23.co:8000/api/v1'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:8000/api/v1',
+  API_URL: 'https://facturacion.og23.co:8000/api/v1',
 };
 
 /*
